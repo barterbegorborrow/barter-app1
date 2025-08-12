cf6335affcdcbe68c85bd17471e2afafa4f5e711
barterbegorborrow <jafenlivevideo@gmail.com>
2025-08-12 01:04:09 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Feed from './components/FeedClient'
+
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Barter Feed</h1>
+      <Feed />
+    </main>
+  )
+}
